fe3659700627d3e247a5f78ec3231080f21ee674
Danny Lai <dannyphlai@gmail.com>
2020-12-06 13:03:30 +1100

    Import dependancies/models into server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//  -------------------- CONNECT TO MONGODB -------------------------------  //
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const express = require('express');
+const jwt = require('jsonwebtoken');
+const jwtAuthenticate = require('express-jwt');
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+const weeklyMenu = require('./models/weeklyMenu');
 const dotenv = require('dotenv').config();
-const app = express();
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,8 +23,22 @@ db.on('error', err => {
   conole.error('Connection error:', err)
 }); //db.on()
 
+//  -------------------- EXPRESS SERVER INITIALISATION --------------------  //
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
 // initialise server that browser can connect to
 app.listen(3000, () => console.log('Listening on 3000'));
+
+// -------------------------------- ROUTES --------------------------------  //
 
 app.get('/', (req, res) => {
   res.send({'hello':'world'})
